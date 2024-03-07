d4e93dc772160b287885091f71455c739ae32239
sara-nevado <sarihds@hotmail.com>
2024-03-07 07:50:17 +0100

    correglo errores archivo join.js

--- staff/sara-nevado/objects/js/join.js
+++ staff/sara-nevado/objects/js/join.js
@@ -15,7 +15,7 @@ function joinElement(object, separador = ',') {
 
 var result = ''
 
-for (var i = =; i < object.length; i++) {
+for (var i = 0; i < object.length; i++) {
     var element = object[i]
 
     //condicion para no agregar la coma al principio
@@ -25,38 +25,71 @@ for (var i = =; i < object.length; i++) {
     }
 
     else {
-        //si i es 0, insertamos el primer elemento en 0
+        //si i es 0, insertamos el primer elemento (alemendro) en resultados
         result = element
+
+
+        //     condicion para no agregar la coma en el final
+        //      if (i !== object.length - 1) {
+        //      result = result + element + separator
+        //       }
+        //      else {
+        //      result = result + element
+        //       }
+
+        return result
     }
 
-    return result
-
-}
-
-console.log('FUNCTION JOIN')
-console.log('CASE 1:join elements from object into an string with default separator  ')
+    console.log('FUNCTION JOIN')
+    console.log('CASE 1:join elements from object into an string with default separator  ","')
 
 
-console.log('CASE 1: join elements from object into an string with default separator ","')
+    var tress = {
+        0: 'almendro',
+        1: 'olivo',
+        2: 'roble',
+        3: 'pino',
+        4: 'abeto',
+        5: 'palmera',
+        6: 'manzano',
+        7: 'arucaria',
+        8: 'limonero',
 
-var tress = {
-    0: 'almendro',
-    1: 'olivo',
-    2: 'roble',
-    3: 'pino',
-    4: 'abeto',
-    5: 'palmera',
-    6: 'manzano',
-    7: 'arucaria',
-    8: 'limonero',
+        length: 9
+    }
 
-    length: 9
+    var result = join(trees)
 
-    var result2 = join(trees, '-')
+
+
+
+    console.log('CASE 1: join elements from object into an string with default separator ","')
+
+    var tress = {
+        0: 'almendro',
+        1: 'olivo',
+        2: 'roble',
+        3: 'pino',
+        4: 'abeto',
+        5: 'palmera',
+        6: 'manzano',
+        7: 'arucaria',
+        8: 'limonero',
+
+        length: 9
+
+    var result = join(trees)
 
     console.log(trees)
-    console.log(result2)
+    console.log(result)
 
+    // expected result = 'Almendro,Olivo,Roble,Pino,Abeto,Palmera,Manzano,Araucaria,Limonero'
 
+console.log('CASE 2': Join elements from object into an string with "-" separator')
 
-}
+var result2 = join(tress, '-')
+
+console.log(trees)
+console.log(result2)
+
+// expected result = 'Almendro-Olivo-Roble-Pino-Abeto-Palmera-Manzano-Araucaria-Limonero'
