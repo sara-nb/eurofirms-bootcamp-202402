52115ee3d28eb50811b7e74d12ad04f7e0a32e6b
sara-nevado <sarihds@hotmail.com>
2024-03-12 12:09:39 +0100

    add split

--- staff/strings/split.js
+++ staff/strings/split.js
@@ -1,10 +1,41 @@
-function split(strin, separator) {
+function split(strin, separator, limit = Infinity) {
+
+    var result = []
+    var acumulatedString = ''
+
+
+    for (var i = 0; < string.length && result.length < limit; i++) {
+
+
+        if (character === separator) {
+            result[result.length] = acumulatedString
+
+            acumulatedString = ''
+        }
 
 
 
+        else {
+            acumulatedString += character
+        }
+
+    }
+
+    if (result.length < limit) {
+        result[result.length] = acumulatedString
+    }
+
+    return result
 
 
 }
 
 console.log('FUNCION SPLIT')
-console.log('CASE 1:')
+
+console.log('CASE 1: split the string with " " separator')
+
+var hello = 'Hello world to the people'
+
+var result = split(hello, ' ', 2)
+
+console.log({ expected: ['Hello', 'world'], received: result })
